66720f462d8e39344e96dc36a72d001c2e62238a
Meltxmicheal <michealjohnsonraj16@gmail.com>
2026-06-28 09:07:39 +0530

    Initial project structure

---
+++ backend/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config.config import Config
+from database.db import db
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    return app
